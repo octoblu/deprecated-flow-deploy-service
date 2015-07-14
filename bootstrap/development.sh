59ec977452d2b6ba4285b7f5282265ed1eb0ec05
Sqrt of Octoblu <sqrt@octoblu.com>
2015-07-14 14:20:28 -0700

    add development bootstrap

--- bootstrap/development.sh
+++ bootstrap/development.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
-etcdctl set /meshblu/host localhost
+echo "Setting etcd defaults for development mode"
+etcdctl set /meshblu/host 172.17.8.1
 etcdctl set /meshblu/port 3000
 etcdctl set /node/env development
-etcdctl set /octoblu/uri http://localhost:8080
+etcdctl set /octoblu/uri http://172.17.8.1:8080
+etcdctl set /flow-deploy-service/base-uri http://172.17.8.1:8899
